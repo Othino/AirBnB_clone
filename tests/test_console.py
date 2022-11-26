40a9f741d831e7f3d391cc5aedee5eb37f42c686
Othino <micahothino44@gmail.com>
2022-11-26 12:53:26 -0800

    Unit test for the console

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,142 +1,53 @@
 #!/usr/bin/python3
 ''' Test suite for the console'''
 
-
 import sys
-import models
 import unittest
-from io import StringIO
+import inspect
+import io
+import pep8
+from contextlib import redirect_stdout
 from console import HBNBCommand
-from unittest.mock import create_autospec
 
 
-class test_console(unittest.TestCase):
-    ''' Test the console module'''
-    def setUp(self):
-        '''setup for'''
-        self.backup = sys.stdout
-        self.capt_out = StringIO()
-        sys.stdout = self.capt_out
+class TestHBNBCommand(unittest.TestCase):
+    """
+    class for testing HBNBCommand class' methods
+    """
 
-    def tearDown(self):
-        ''''''
-        sys.stdout = self.backup
+    @classmethod
+    def setUpClass(cls):
+        """
+        Set up class method for the doc tests
+        """
+        cls.setup = inspect.getmembers(HBNBCommand, inspect.isfunction)
 
-    def create(self):
-        ''' create an instance of the HBNBCommand class'''
-        return HBNBCommand()
+    def test_pep8_conformance_HBNBCommand(self):
+        """
+        Test that console.py file conform to PEP8
+        """
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/console.py'])
+        self.assertEqual(result.total_errors, 1,
+                         "Found code style errors (and warnings).")
 
-    def test_quit(self):
-        ''' Test quit exists'''
-        console = self.create()
-        self.assertTrue(console.onecmd("quit"))
+    def test_pep8_conformance_test_HBNBCommand(self):
+        """
+        Test that test_console.py file conform to PEP8
+        """
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['tests/test_models/test_console.py'])
+        self.assertEqual(result.total_errors, 1,
+                         "Found code style errors (and warnings).")
 
-    def test_EOF(self):
-        ''' Test EOF exists'''
-        console = self.create()
-        self.assertTrue(console.onecmd("EOF"))
+    def test_module_docstring(self):
+        """
+        Tests if module docstring documentation exist
+        """
+        self.assertTrue(len(HBNBCommand.__doc__) >= 1)
 
-    def test_all(self):
-        ''' Test all exists'''
-        console = self.create()
-        console.onecmd("all")
-        self.assertTrue(isinstance(self.capt_out.getvalue(), str))
-
-    def test_show(self):
-        '''
-            Testing that show exists
-        '''
-        console = self.create()
-        console.onecmd("create User")
-        user_id = self.capt_out.getvalue()
-        sys.stdout = self.backup
-        self.capt_out.close()
-        self.capt_out = StringIO()
-        sys.stdout = self.capt_out
-        console.onecmd("show User " + user_id)
-        x = (self.capt_out.getvalue())
-        sys.stdout = self.backup
-        self.assertTrue(str is type(x))
-
-    def test_show_class_name(self):
-        '''
-            Testing the error messages for class name missing.
-        '''
-        console = self.create()
-        console.onecmd("create User")
-        user_id = self.capt_out.getvalue()
-        sys.stdout = self.backup
-        self.capt_out.close()
-        self.capt_out = StringIO()
-        sys.stdout = self.capt_out
-        console.onecmd("show")
-        x = (self.capt_out.getvalue())
-        sys.stdout = self.backup
-        self.assertEqual("** class name missing **\n", x)
-
-    def test_show_class_name(self):
-        '''
-            Test show message error for id missing
-        '''
-        console = self.create()
-        console.onecmd("create User")
-        user_id = self.capt_out.getvalue()
-        sys.stdout = self.backup
-        self.capt_out.close()
-        self.capt_out = StringIO()
-        sys.stdout = self.capt_out
-        console.onecmd("show User")
-        x = (self.capt_out.getvalue())
-        sys.stdout = self.backup
-        self.assertEqual("** instance id missing **\n", x)
-
-    def test_show_no_instance_found(self):
-        '''
-            Test show message error for id missing
-        '''
-        console = self.create()
-        console.onecmd("create User")
-        user_id = self.capt_out.getvalue()
-        sys.stdout = self.backup
-        self.capt_out.close()
-        self.capt_out = StringIO()
-        sys.stdout = self.capt_out
-        console.onecmd("show User " + "124356876")
-        x = (self.capt_out.getvalue())
-        sys.stdout = self.backup
-        self.assertEqual("** no instance found **\n", x)
-
-    def test_create(self):
-        '''
-            Test that create works
-        '''
-        console = self.create()
-        console.onecmd("create User")
-        self.assertTrue(isinstance(self.capt_out.getvalue(), str))
-
-    def test_class_name(self):
-        '''
-            Testing the error messages for class name missing.
-        '''
-        console = self.create()
-        console.onecmd("create")
-        x = (self.capt_out.getvalue())
-        self.assertEqual("** class name missing **\n", x)
-
-    def test_class_name_doest_exist(self):
-        '''
-            Testing the error messages for class name missing.
-        '''
-        console = self.create()
-        console.onecmd("create Binita")
-        x = (self.capt_out.getvalue())
-        self.assertEqual("** class doesn't exist **\n", x)
-
-    '''
-    def test_destroy(self):
-        console = self.create()
-        self.assertTrue(console.onecmd("destroy"))
-    def test_update(self):
-        console = self.create()
-        self.assertTrue(console.onecmd("update"))
-    '''
+    def test_class_docstring(self):
+        """
+        Tests if class docstring documentation exist
+        """
+        self.assertTrue(len(HBNBCommand.__doc__) >= 1)
